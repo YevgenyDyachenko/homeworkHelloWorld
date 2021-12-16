a1e61435f428e1da385054f09b706aee227aa8ab
YevgenyDyachenko <77688016+YevgenyDyachenko@users.noreply.github.com>
2021-12-16 16:02:49 +0200

    Create View.java

---
+++ View.java
@@ -0,0 +1,16 @@
+package mvc;
+
+public class View {
+
+    public static final String INPUT_STRING_DATA = "Input a word 'Hello': ";
+    public static final String INPUT_STRING_DATA1 = "Input a word 'world': ";
+    public static final String WRONG_INPUT_REPEAT_PLEASE = "Wrong input! Repeat please! ";
+    public static final String PHRASE_IS = "Our phrase is: ";
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+    public void printMessageAndOther(String message, String value){
+        System.out.println(message + value);
+    }
+}
